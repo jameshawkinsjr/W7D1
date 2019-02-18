9f35426d02bc986ede382ee74373ccd63fbd270c
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-02-18 15:25:23 -0800

    Complete phase 2 Todo Project

--- todos_project/frontend/todo_redux.jsx
+++ todos_project/frontend/todo_redux.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configureStore } from './store/store'
 
+const store = configureStore();
+window.store = store;
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
