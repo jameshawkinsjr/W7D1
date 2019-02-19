d029120cf8f2a31948e5ccded3f03c8cdb76a88e
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-02-18 16:48:12 -0800

    Add Todo form

--- todos_project/frontend/todo_redux.jsx
+++ todos_project/frontend/todo_redux.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { configureStore } from './store/store'
+import App from './components/app';
+import Root from './components/root';
+import allTodos from './reducers/selectors'
 
 const store = configureStore();
 window.store = store;
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Its working.!</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
